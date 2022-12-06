fb518122dfa13f14f4ced6ae6d8f88c74be7234f
Niall Barber <niall.barber@olioex.com>
2022-12-06 19:32:44 +0000

    Split code out

--- rust/day_01/src/main.rs
+++ rust/day_01/src/main.rs
@@ -1,10 +1,14 @@
 use std::fs;
 
+pub fn get_data() -> String {
+    let data = fs::read_to_string("./src/data.txt").expect("Error reading file");
+    return data;
+}
+
 fn main() {
     let mut total = 0;
     let mut highest_score: Vec<i32> = Vec::new();
-    let data = fs::read_to_string("./src/data.txt").expect("Error reading file");
-
+    let data = get_data();
     let formatted_data: Vec<&str> = data.split("\n").collect();
 
     for item in formatted_data {
