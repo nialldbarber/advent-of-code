07d4122f03261d3b6678697d478104624f34ef2a
Niall Barber <niall.barber@olioex.com>
2023-12-02 08:40:09 +0000

    Day 2 setup

--- 2023/01/challenge.md
+++ 2023/01/challenge.md
@@ -1,4 +1,5 @@
---- Day 1: Trebuchet?! ---
+# Day 1: Trebuchet?!
+
 Something is wrong with global snow production, and you've been selected to take a look. The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
 
 You've been doing this long enough to know that to restore snow operations, you need to check all fifty stars by December 25th.
--- 2023/01/index.ts
+++ 2023/01/index.ts
@@ -1,61 +1,75 @@
-export const formatString = (data: string) => data.split(/\r?\n/);
+export const formatString = (data: string) => data.split(/\r?\n/)
 
 export function stripNonDigits(value: string | number) {
-	if (typeof value === "string") {
-		return value.replace(/\D+/g, "");
-	}
-	return value.toString().replace(/\d+/g, "");
+  if (typeof value === 'string') {
+    return value.replace(/\D+/g, '')
+  }
+  return value.toString().replace(/\d+/g, '')
 }
 
 export function stripNonNumbers(value: string | number) {
-	if (typeof value === "string") {
-		return value.replace(
-			/\b(?!one|two|three|four|five|six|seven|eight|nine\b)\w+\b/gi,
-			"",
-		);
-	}
-	return value;
+  if (typeof value === 'string') {
+    const numberWordsToDigits: Record<string, string> = {
+      one: '1',
+      two: '2',
+      three: '3',
+      four: '4',
+      five: '5',
+      six: '6',
+      seven: '7',
+      oneight: '8',
+      eight: '8',
+      nine: '9',
+    }
+
+    return value.replace(
+      /oneight|one|two|three|four|five|six|seven|eight|nine/gi,
+      (matched) => {
+        return numberWordsToDigits[matched.toLowerCase()]
+      }
+    )
+  }
+  return value.toString()
 }
 
 export function getBookends(str: string) {
-	return `${str.at(0)}${str.at(-1)}`;
+  return `${str.at(0)}${str.at(-1)}`
 }
 
 export async function readInputFromFile() {
-	try {
-		const file = Bun.file("input.txt");
-		const data = await file.text();
-		return formatString(data);
-	} catch (error) {
-		console.error(error);
-	}
+  try {
+    const file = Bun.file('input.txt')
+    const data = await file.text()
+    return formatString(data)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 async function accumulateValuesPartOne() {
-	try {
-		const data = await readInputFromFile();
-		return data?.reduce((total, curr) => {
-			const bookends = getBookends(stripNonDigits(curr));
-			return total + Number(bookends);
-		}, 0);
-	} catch (error) {
-		console.error(error);
-	}
+  try {
+    const data = await readInputFromFile()
+    return data?.reduce((total, curr) => {
+      const bookends = getBookends(stripNonDigits(curr))
+      return total + Number(bookends)
+    }, 0)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 async function accumulateValuesPartTwo() {
-	try {
-		const data = await readInputFromFile();
-		return data?.reduce((total, curr) => {
-			console.log(stripNonNumbers(curr));
-
-			// const bookends = getBookends(stripNonNumbers(curr));
-			// return total + Number(bookends);
-		}, 0);
-	} catch (error) {
-		console.error(error);
-	}
+  try {
+    const data = await readInputFromFile()
+    return data?.reduce((total, curr) => {
+      const formatNumbers = stripNonNumbers(curr)
+      const bookends = getBookends(stripNonDigits(formatNumbers))
+      return total + Number(bookends)
+    }, 0)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
-// accumulateValuesPartOne().then(console.log);
-accumulateValuesPartTwo().then(console.log);
+accumulateValuesPartOne().then(console.log)
+accumulateValuesPartTwo().then(console.log)
